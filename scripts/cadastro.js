074dfa9d07d1738baa9be734380d097cad6e963a
gabrielgiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-05-06 09:20:21 -0300

    add - cadastro js

---
+++ scripts/cadastro.js
@@ -0,0 +1,19 @@
+document.getElementById("FormLogin").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("email").value;
+    let confirm_senha = document.getElementById("confirm_senha").value;
+    let estado = document.getElementById("EstadoCivil").value;
+    let nome = document.getElementById("nome").value;
+    let cpf = document.getElementById("cpf").value;
+    let data = document.getElementById("data").value;
+    let cep = document.getElementById("cep").value;
+    let rua = document.getElementById("rua").value;
+    let comp = document.getElementById("comp").value;
+    let cidade = document.getElementById("cidade").value;
+    let ramal = document.getElementById("ramal").value;
+
+    
+});
