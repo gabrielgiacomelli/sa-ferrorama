181af71edade56bd2eebde12ae422b073059300a
gabrielgiacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-05-20 11:42:47 -0300

    add - login js

---
+++ scripts/login.js
@@ -0,0 +1,29 @@
+document.getElementById("FormLogin").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let erro = document.getElementById("problema");
+
+    let emailCorreto = "adm@gmail.com";
+    let senhaCorreta = "1234";
+
+    if (email === "" || senha === "") {
+        erro.textContent = "Preencha todos os campos!";
+        return;
+    }
+
+    if (email !== emailCorreto || senha !== senhaCorreta) {
+        erro.textContent = "Email ou senha incorretos!";
+        return;
+    }
+
+    window.location.href = "home.html";
+
+
+});
+
+function esqueceuSenha() {
+    alert("Entre em contato com o suporte para recuperar sua senha.");
+}
