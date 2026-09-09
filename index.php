b07040940c00ee353f082c8e9ae44a20a486e5a7
Henrique_Venso <henrique_venso@estudante.sesisenai.org.br>
2026-09-09 11:04:19 -0300

    ref - Início da refatoração do index

--- index.php
+++ index.php
@@ -1,44 +1,10 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela Inicial</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles/style.css">
-
+    <title> Página Inicial </title>
 </head>
-
-<body class="body_index">
-
-    <main>
-        <!-- Main com imagem clicável, redireciona para a página de login -->
-        <a href="public/login.html">
-
-            <!-- Imagem principal da tela inicial -->
-            <img id="login-link" src="assets/img/telainicial.png" alt="Imagem Tela Inicial">
-
-        </a>
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
-
-    <script>
-        document.getElementById('login-link').addEventListener('click', function (e) {
-            e.preventDefault(); // impede navegação imediata
-
-            const img = this;
-            img.classList.add('animate'); // dispara a animação CSS
-
-            // Aguarda a animação terminar (600ms) antes de navegar
-            setTimeout(() => {
-                window.location.href = 'public/login.html';
-            }, 600);
-        });
-    </script>
-
+<body>
+    
 </body>
-
 </html>
