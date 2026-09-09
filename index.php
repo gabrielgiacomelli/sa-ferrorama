d326c5bfab369e695c6cf292f75923d8640482b7
Henrique_Venso <henrique_venso@estudante.sesisenai.org.br>
2026-09-09 11:34:53 -0300

    up - Atualizei o nav link que restava

--- index.php
+++ index.php
@@ -31,16 +31,16 @@
                     <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
 
                         <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="home.html">Home</a>
+                            <a class="nav-link mx-lg-2" href="home.php">Home</a>
                         </li>
 
                         <li class="nav-item dropdown">
                             <a class="nav-link mx-lg-2">Sensores</a>
 
                             <div class="dropdown-menu">
-                                <a class="nav-link mx-lg-2" href="sensores-ferrovia.html"> Visualizar Sensores Ferrovia
+                                <a class="nav-link mx-lg-2" href="sensores-ferrovia.php"> Visualizar Sensores Ferrovia
                                 </a>
-                                <a class="nav-link mx-lg-2" href="sensores-trem.html"> Visualizar Sensores Trem </a>
+                                <a class="nav-link mx-lg-2" href="sensores-trem.php"> Visualizar Sensores Trem </a>
                             </div>
 
                         </li>
@@ -50,17 +50,17 @@
 
                             <div class="dropdown-menu">
 
-                                <a class="nav-link mx-lg-2" href="cadastro-sensor.html"> Cadastro de Sensores </a>
-                                <a class="nav-link mx-lg-2" href="cadastro-trem.html"> Cadastro de Trens </a>
-                                <a class="nav-link mx-lg-2" href="cadastro-rota.html"> Cadastro de Rotas </a>
-                                <a class="nav-link mx-lg-2" href="cadastro-usuario.html"> Cadastro de Usuários </a>
+                                <a class="nav-link mx-lg-2" href="cadastro-sensor.php"> Cadastro de Sensores </a>
+                                <a class="nav-link mx-lg-2" href="cadastro-trem.php"> Cadastro de Trens </a>
+                                <a class="nav-link mx-lg-2" href="cadastro-rota.php"> Cadastro de Rotas </a>
+                                <a class="nav-link mx-lg-2" href="cadastro-usuario.php"> Cadastro de Usuários </a>
 
                             </div>
 
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="monitoramento.html">Monitoramento</a>
+                            <a class="nav-link mx-lg-2" href="monitoramento.php">Monitoramento</a>
                         </li>
 
                         <li class="nav-item dropdown">
@@ -68,15 +68,15 @@
 
                             <div class="dropdown-menu">
 
-                                <a class="nav-link mx-lg-2 active fw-bold" aria-current="page" href="cadastro-relatorio.html"> Cadastro de Relatórios </a>
-                                <a class="nav-link mx-lg-2" href="relatorios.html"> Visualizar Relatórios </a>
+                                <a class="nav-link mx-lg-2 active fw-bold" aria-current="page" href="cadastro-relatorio.php"> Cadastro de Relatórios </a>
+                                <a class="nav-link mx-lg-2" href="relatorios.php"> Visualizar Relatórios </a>
 
                             </div>
 
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="usuarios.html">Usuários</a>
+                            <a class="nav-link mx-lg-2" href="usuarios.php">Usuários</a>
                         </li>
 
                     </ul>
