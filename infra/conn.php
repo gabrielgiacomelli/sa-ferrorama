1bc42c7d3848dfa5ac799ffa3348e6f8ef234078
Gabriel da Costa Giacomelli <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-09-09 11:40:18 -0300

    add - conexão feita

--- infra/conn.php
+++ infra/conn.php
@@ -1,3 +1,15 @@
 <?php
 
+$host = "localhost";
+$user = "root";
+$password = "";
+$banco = "sa_ferrorama_db";
+
+$conn =  new mysqli($host, $user, $password, $banco);
+
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco " . $conn->connect_error);
+};
+
+$conn->set_charset("utf8mb4");
 ?>
