e11d636b7635c65252e5acea529a609fba93018f
Lucas S. Correa <lucas_correa14@estudante.sesisenai.org.br>
2026-09-09 09:59:08 -0300

    ref - começo da refatoração da navbar

---
+++ navbar.php
@@ -0,0 +1,32 @@
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <header >
+            <div>
+        
+                <div id="navbar-ferroviario-contraste">
+                    <div id="navbar-ferroviario">
+
+                        <nav >
+                            <ul id="navbar-flex">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Cadastro</a></li>
+                                <li><a href="#">Gestão</a></li>
+                                <li><a href="#">Monitoramento</a></li>
+                                <li><a href="#">Sair</a></li>
+                            </ul>
+                        </nav>
+
+                    </div>
+                </div>
+
+            </div>
+        </main>
+    </header>
+</body>
+</html>
