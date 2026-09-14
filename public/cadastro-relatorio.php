0bac8be74088c22d1f64b291f9f832ab00c84a39
Henrique_Venso <henrique_venso@estudante.sesisenai.org.br>
2026-09-14 11:27:44 -0300

    up - Atualizei o cadastro-relatorio e relatorios

--- public/cadastro-relatorio.php
+++ public/cadastro-relatorio.php
@@ -15,110 +15,46 @@ mysqli_query($conn, $sql);
 
 ?>
 
+
+
 <html lang="en">
+<?php
+$paginaAtual = "cadastro";
+$submenuAtual = "relatorios";
+include("navbar.php");
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro de Relatórios</title>
+<body>
 
-    <link rel="stylesheet" href="../styles/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-
-</head>
-
-<body id="body2">
-
-    <!-- Início do header -->
-
-<nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand me-auto">
-                <img class="logo navbar-brand me-auto" src="../assets/icons/logo.png" alt="LOGO">
-            </a>
-            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
-                <div class="offcanvas-header">
-                    <img class="logo" src="../assets/icons/logo.png" alt="LOGO">
-                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                </div>
-                <div class="offcanvas-body">
-                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
-
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="home.php">Home</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link mx-lg-2">Sensores</a>
-
-                            <div class="dropdown-menu">
-                                <a class="nav-link mx-lg-2" href="sensores-ferrovia.php"> Visualizar Sensores Ferrovia
-                                </a>
-                                <a class="nav-link mx-lg-2" href="sensores-trem.php"> Visualizar Sensores Trem </a>
-                            </div>
-
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link mx-lg-2">Cadastro</a>
-
-                            <div class="dropdown-menu">
-
-                                <a class="nav-link mx-lg-2" href="cadastro-sensor.php"> Cadastro de Sensores </a>
-                                <a class="nav-link mx-lg-2" href="cadastro-trem.php"> Cadastro de Trens </a>
-                                <a class="nav-link mx-lg-2" href="cadastro-rota.php"> Cadastro de Rotas </a>
-                                <a class="nav-link mx-lg-2" href="cadastro-usuario.php"> Cadastro de Usuários </a>
-
-                            </div>
-
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="monitoramento.php">Monitoramento</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link mx-lg-2 active fw-bold" aria-current="page">Relatorios</a>
-
-                            <div class="dropdown-menu">
-
-                                <a class="nav-link mx-lg-2 active fw-bold" aria-current="page" href="cadastro-relatorio.php"> Cadastro de Relatórios </a>
-                                <a class="nav-link mx-lg-2" href="relatorios.php"> Visualizar Relatórios </a>
-
-                            </div>
-
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link mx-lg-2" href="usuarios.php">Usuários</a>
-                        </li>
-
-                    </ul>
-                </div>
-            </div>
-            <div class="sair-headder">
-                <img id="img-3" src="../assets/icons/sair.png" alt="Imagem sair">
-                <button id="open-popup" class="nav-link mx-lg-2">Sair</button>
-            </div>
-        </div>
-        </div>
-    </nav>
-
-    <!-- Fim do header -->
 
 <main>
 
-<form method = "POST">
+<div class="container d-flex justify-content-center align-items-center" style="min-height: 100vh;">
 
-<label for="conteudo"></label>
-<input type="text" name ="conteudo">
-</input>
+        <!--define o layout responsivo de um elemento.-->
+        <div class="col-lg-11">
 
-<button> Cadastrar </button>
+            <!--card estilizado com espaçamento interno, borda cinza, cantos arredondados e sombra projetada-->
+            <div class="card p-5 shadow-lg border-color: gray  rounded-4">
 
-</form>
+                <!--título do cadastro, centralizado horizontalmente e com espaçamento inferior-->
+                <h2 class="text-center mb-4">Cadastro de Usuários</h2>
+
+                <!--formulário de cadastro, organizado em uma grade com espaçamento entre os elementos-->
+                <form method = "POST">
+                    <div class="row g-4">
+
+                        <div class="col-md-4">
+
+                            <!--campo de email-->
+
+                            <label for="conteudo" class="form-label"> Relatórios </label>
+                            <input id="conteudo" type="text" placeholder="Digite seu relatório" name = "conteudo">
+                            </input>
+
+                        <button>Cadastrar</button>
+
+                </form>
 
 </main>
 
