2eb5c6e66a690ab31872f68f6b62b14c3abe9484
Henrique_Venso <henrique_venso@estudante.sesisenai.org.br>
2026-09-14 11:53:08 -0300

    up - Atualizei a conexão

--- infra/conn.php
+++ infra/conn.php
@@ -2,16 +2,10 @@
 
 $host = "localhost";
 $user = "root";
-<<<<<<< HEAD
 $password = "root";
 $banco = "sa_ferrorama_db";
 
 $conn =  new mysqli($host, $user, $password, $banco);
-=======
-$banco = "sa_ferrorama_db";
-
-$conn = @new mysqli($host, $user, "", $banco, 3307);
->>>>>>> b2dba9e004af6e50f4c48086d91c0cb7145945f6
 
 
 if ($conn->connect_error){
