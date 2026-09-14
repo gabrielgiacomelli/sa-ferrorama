b2dba9e004af6e50f4c48086d91c0cb7145945f6
Lucas S. Correa <lucas_correa14@estudante.sesisenai.org.br>
2026-09-14 11:45:45 -0300

    up - atualizei o conn.php

--- infra/conn.php
+++ infra/conn.php
@@ -2,14 +2,18 @@
 
 $host = "localhost";
 $user = "root";
-$password = "";
 $banco = "sa_ferrorama_db";
 
-$conn =  new mysqli($host, $user, $password, $banco,3307);
+$conn = @new mysqli($host, $user, "", $banco, 3307);
 
-if ($conn->connect_error) {
-    die("Erro na conexão com o banco " . $conn->connect_error);
-};
+
+if ($conn->connect_error){
+    $conn = @new mysqli($host, $user, "root", $banco);
+    
+    if ($conn->connect_error) {
+        die("Erro na conexão com o banco " . $conn->connect_error);
+    };
+}
 
 $conn->set_charset("utf8mb4");
 ?>
