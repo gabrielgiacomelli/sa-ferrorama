53367a067bb5bf2cab17f9d8a799148f9bc89722
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-09-23 08:58:25 -0300

    up - nome das paginas

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial - HLGL</title>
+    <title>Página Inicial</title>
     <link rel="stylesheet" href="styles/style.css">
 </head>
 <body id="index-page">
--- public/home.php
+++ public/home.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home - HLGL</title>
+    <title>Home</title>
     <link rel="stylesheet" href="../styles/style.css">
 </head>
 
