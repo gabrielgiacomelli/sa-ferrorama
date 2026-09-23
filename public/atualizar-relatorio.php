f3a63e1b71f3ea3c6302b209fef1629b89d931f5
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-09-23 09:29:29 -0300

    add - editar e atualizar relatório

---
+++ public/atualizar-relatorio.php
@@ -0,0 +1,14 @@
+<?php
+
+include "../infra/conn.php";
+$id = $_POST["id"];
+$conteudo = $_POST["conteudo"];
+
+$sql = "UPDATE relatorios SET conteudo=? WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("si", $conteudo, $id);
+$stmt->execute();
+
+header("Location: gestao-relatorios.php");
+
+?>
--- public/editar-relatorio.php
+++ public/editar-relatorio.php
@@ -19,7 +19,62 @@ $relatorio = mysqli_fetch_assoc($resultado);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar Relatório</title>
 </head>
+
 <body>
-    
+
+    <main id="cadastro-relatorios">
+
+        <h2>Editar Relatório</h2>
+
+        <div class="container">
+
+            <div class="col-lg-11">
+
+                <div class="card">
+
+                    <form
+                        action="atualizar-relatorio.php"
+                        method="POST"
+                    >
+
+                        <input
+                            type="hidden"
+                            name="id"
+                            value="<?php echo $relatorio["id"]; ?>"
+                        >
+
+                        <div class="row">
+
+                            <div class="col-md-4">
+
+                                <textarea
+                                    id="conteudo"
+                                    name="conteudo"
+                                    placeholder="Escreva seu relatório"
+                                    required
+                                ><?php echo htmlspecialchars($relatorio["conteudo"]); ?></textarea>
+
+
+                                <button type="submit">
+                                    Atualizar
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </main>
+
+
+    <script src="../scripts/botao-sair.js"></script>
+
 </body>
 </html>
