e22e1726a9f7f0589de334d8cb4e21e4148f10a8
LaraGuzman <lara_guzman@estudante.sesisenai.org.br>
2026-09-25 11:59:59 -0300

    fix - arrumando nomes das variáveis

--- public/atualizar-trem.php
+++ public/atualizar-trem.php
@@ -3,12 +3,12 @@
 include "../infra/conn.php";
 $id = $_POST["id"];
 $peso = $_POST["peso"];
-$quantidade = $_POST["quantidade"];
-$proprietario = $_POST["proprietario"];
+$quantidade_vagoes = $_POST["quantidade_vagoes"];
+$id_usuarios = $_POST["id_usuarios"];
 
-$sql = "UPDATE trens SET peso=?, quantidade=?, proprietario=? WHERE id = ?";
+$sql = "UPDATE trens SET peso=?, quantidade_vagoes=?, id_usuarios=? WHERE id = ?";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("diii", $peso, $quantidade, $proprietario, $id);
+$stmt->bind_param("diii", $peso, $quantidade_vagoes, $id_usuarios, $id);
 $stmt->execute();
 
 header("Location: gestao-relatorios.php?sucesso=trem");
